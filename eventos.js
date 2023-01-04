16d63b3a8ee91a243d0933539948c57016da29c0
Igor Altino <igor.altino@hotmail.com>
2023-01-04 14:56:23 -0300

    Criando elementos via JS

--- eventos.js
+++ eventos.js
@@ -1,4 +1,4 @@
-let conta = 0; 
+let conta = 3; 
 
 
 let clique = document.querySelector("button");
@@ -6,5 +6,40 @@ let clique = document.querySelector("button");
 clique.addEventListener("click",() => {
     conta++;
     document.querySelector("h1").innerHTML = `Você clicou ${conta} vezes`;
+    //document.querySelector("#teste ul li").append("Teste13");
+    let newItem = document.createElement("li");
+    newItem.innerText = `Item ${conta}`;
+    document.querySelector("#teste ul").append(newItem);
 });
+
+//document.querySelector("#teste ul").innerHTML += "<li> Testando...</li>";
+
+
+
+
+let clique = document.querySelector('button');
+
+clique.addEventListener('click',() =>{
+    document.querySelector()
+})
+
+
+
+
+let newUl = document.createElement("ul");
+for(let i =0 ;i <5;i++){
+    let newLi = document.createElement("li");
+    newLi.innerText = "Novo li";
+    newUl.append(newLi);
+}
+
+document.querySelector("ul").before(newUl);
+
+let input = document.createElement("input");
+document.querySelector("button").after(input);
+//setando atributos no input
+document.querySelector("input").setAttribute('placeholder','nome');
+
+
+
 
